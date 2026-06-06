50483526f875c549adb7e7a36257f72eb083388f
augusto1507 <99281591+augusto1507@users.noreply.github.com>
2026-06-06 18:11:38 -0300

    config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ export const routes: Routes = [
     {
         path: "login-cadastro", loadComponent: () =>
             import('./pages/login-cadastro/login-cadastro').then(x => x.LoginCadastro)
+    },
+
+    {
+        path: "configuracao", loadComponent: () =>
+            import('./pages/configuracao/configuracao').then(x => x.Configuracao)
     }
 ];
 
