e1eca25abc281e7aa453d0ba315e99f6efafb844
augusto1507 <99281591+augusto1507@users.noreply.github.com>
2026-06-06 16:14:49 -0300

    tela de login e cadastro feita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login-cadastro", loadComponent: () =>
+            import('./pages/login-cadastro/login-cadastro').then(x => x.LoginCadastro)
+    }
+];
+
---
+++ src/app/pages/login-cadastro/login-cadastro.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login-cadastro',
+  imports: [RouterLink],
+  templateUrl: './login-cadastro.html',
+  styleUrl: './login-cadastro.css',
+})
+export class LoginCadastro {
+
+  constructor( private router: Router){}
+
+    mostrarCadastro = false;
+  
+  
+    trocarTela() {
+      this.mostrarCadastro = !this.mostrarCadastro;
+    }
+
+    logar(){
+      this.router.navigate(["./"])
+    }
+
+    cadastrar(){
+      this.router.navigate(["./"])
+    }
+  
+  
+}
